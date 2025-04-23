e642c0cb887ab128b54838764eaa56ecf396e3db
mavor <ma_voroshilov@mail.ru>
2025-04-23 22:24:20 +0300

    Remove commented code

--- lab10/ping/ping.py
+++ lab10/ping/ping.py
@@ -27,25 +27,6 @@ def get_checksum(data):
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
 
-# def get_checksum(source_string):
-#     sum = 0
-#     count_to = (len(source_string) // 2) * 2
-#     count = 0
-#     while count < count_to:
-#         this_val = source_string[count + 1]*256+source_string[count]
-#         sum = sum + this_val
-#         sum = sum & 0xffffffff
-#         count = count + 2
-#     if count_to < len(source_string):
-#         sum = sum + source_string[len(source_string) - 1]
-#         sum = sum & 0xffffffff
-#     sum = (sum >> 16) + (sum & 0xffff)
-#     sum = sum + (sum >> 16)
-#     answer = ~sum
-#     answer = answer & 0xffff
-#     answer = answer >> 8 | (answer << 8 & 0xff00)
-#     return answer
-
 
 if len(sys.argv) != 3:
     print("Provide number of requests and host name")
