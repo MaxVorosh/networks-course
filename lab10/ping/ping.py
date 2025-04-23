198196f6306db3296e52a7b322dc7c40839ea8d5
mavor <ma_voroshilov@mail.ru>
2025-04-23 22:40:45 +0300

    Complete ping

--- lab10/ping/ping.py
+++ lab10/ping/ping.py
@@ -32,6 +32,29 @@ if len(sys.argv) != 3:
     print("Provide number of requests and host name")
     sys.exit(1)
 
+msgs = {3: {
+            0: 'Сеть недостижима',
+            1: 'Узел недостижим',
+            2: 'Протокол недостижим',
+            3: 'Порт недостижим',
+            4: 'Необходима фрагментация, но установлен флаг её запрета (DF)',
+            5: 'Неверный маршрут от источника',
+            6: 'Сеть назначения неизвестна',
+            7: 'Узел назначения неизвестен',
+            8: 'Узел источник изолирован',
+            9: 'Сеть административно запрещена',
+            10: 'Узел административно запрещён',
+            11: 'Сеть недоступна для ToS',
+            12: 'Узел недоступен для ToS',
+            13: 'Коммуникации административно запрещены',
+            14: 'Нарушение порядка предпочтения узлов',
+            15: 'Активно отсечение порядка предпочтения'
+            },
+        11: {
+            0: 'Время жизни пакета (TTL) истекло при транспортировке',
+            1: 'Время жизни пакета истекло при сборке фрагментов'
+            }
+        }
 tries = int(sys.argv[1])
 host_name = sys.argv[2]
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname('icmp'))
@@ -48,6 +71,11 @@ for i in range(tries):
         resp, addr = clientSocket.recvfrom(1024)
         time = datetime.datetime.now()
         header = resp[20:28]
+        type, code = header[0], header[1]
+        if type == 3 or type == 11:
+            lost += 1
+            print(msgs[type][code])
+            continue
         data = resp[28:]
         resp_time = datetime.datetime.strptime(data.decode('utf-8'), "%Y-%m-%d %H:%M:%S.%f")
         diff = time - resp_time
@@ -55,12 +83,13 @@ for i in range(tries):
         min_rtt = min(min_rtt, rtt)
         max_rtt = max(max_rtt, rtt)
         sum_rtt += rtt
-        print("ping")
+        print(f"Min rtt: {min_rtt}ms")
+        print(f"Max rtt: {max_rtt}ms")
+        print(f"Avg rtt: {sum_rtt / (i + 1)}ms")
+        print(f"Lost: {lost / (i + 1) * 100}%")
+        print()
     except Exception as e:
         lost += 1
         print(e)
-    print(f"Min rtt: {min_rtt}ms")
-    print(f"Max rtt: {max_rtt}ms")
-    print(f"Avg rtt: {sum_rtt / (i + 1)}ms")
-    print(f"Lost: {lost / (i + 1) * 100}%")
+        print()
 clientSocket.close()
